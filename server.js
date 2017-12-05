d06c8986a98ba7d31ac547f0569086f01dc4a871
Brennen Lowe <lowebrennen@gmail.com>
2017-12-05 10:17:05 -0800

    fixed something on serverjs

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ var PORT = 3000;
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function ()  {
+    console.log("App listening on PORT " + PORT)
+});
